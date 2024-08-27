b6e87961f7a56ee5e40e617b051e63d2d0dc7c2f
Muhibul Haque <muhib.b4m@gmail.com>
2024-08-27 15:23:49 +0600

    Movie Rating

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 
-import StarRating from './components/StarRating';
-import Home from './components/Home';
+// import StarRating from './components/StarRating';
+// import Home from './components/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Home />
+    <App />
+    {/* <Home />
     <StarRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}/>
-    <StarRating maxRating={5} color='green' size={24} className='test' messages={["One", "Two", "Three", "Four", "Five"]} defaultRating={4}/>
+    <StarRating maxRating={5} color='green' size={24} className='test' messages={["One", "Two", "Three", "Four", "Five"]} defaultRating={4}/> */}
   </React.StrictMode>
 );
  
